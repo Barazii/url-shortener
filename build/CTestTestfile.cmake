d6fe2f59d664a609bdd3b3ffc92aacfe78878f89
Barazii <mahmoud.baraziii@gmail.com>
2025-11-24 02:27:52 +0100

    deleted files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mahmood/url-shortener
-# Build directory: /home/mahmood/url-shortener/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ShortenerBasic "/home/mahmood/url-shortener/build/shortener_tests")
-set_tests_properties(ShortenerBasic PROPERTIES  _BACKTRACE_TRIPLES "/home/mahmood/url-shortener/CMakeLists.txt;34;add_test;/home/mahmood/url-shortener/CMakeLists.txt;0;")
